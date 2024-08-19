d045d6869f9933f9b0c46b4aca6733742a42cb1a
mahmoud <mahmoudadel3010@icould.com>
2024-08-19 23:10:38 +0300

    gitTest

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-FTPKMTJI.mjs";import"./chunk-NZ2ET2RY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { BlankLayoutComponent } from './layouts/blank-layout/blank-layout.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { LoginComponent } from './components/login/login.component';
+import { combineLatest } from 'rxjs';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:AuthLayoutComponent,children:[
+        {path:'login',component:LoginComponent},
+        {path:'register',component:RegisterComponent}
+    ]},
+    {path:'',component:BlankLayoutComponent,children:[
+        {path:'',redirectTo:'home',pathMatch:'full'},
+        {path:'home',component:HomeComponent},
+        {path:'cart',component:CartComponent},
+        {path:'brands',component:BrandsComponent},
+        {path:'categories',component:CategoriesComponent},
+    ]},
+    {path:'**',component:NotfoundComponent}
+];
